213f4f1e1e77c417e98a4fbb82905aab2eeb66cc
Dhanraj Raghunathan <draghun@ncsu.edu>
2020-10-22 23:27:48 -0400

    Fix landing page missed '='

--- src/components/main.js
+++ src/components/main.js
@@ -8,8 +8,8 @@ import Resume from './resume'
 
 const Main = () => (
     <Switch>
-        <Route exact path="/react_portfolio/" component={LandingPage}/>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/react_portfolio" component={LandingPage}/>
         <Route path="/aboutme" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Projects} />
